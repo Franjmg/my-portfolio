8022ffffae80ffb34a77ce1b8b248383d14831bd
Franjmg <franjmg@fi.unju.edu.ar>
2022-05-21 06:37:30 -0300

    new design for mobile on the contact page

--- main.js
+++ main.js
@@ -35,4 +35,13 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-
+/* INPUT CONTACT */
+document.querySelectorAll(".text-input").forEach((element) => {
+    element.addEventListener("blur", (event) => {
+        if (event.target.value != "") {
+            event.target.nextElementSibling.classList.add("filled");
+        } else {
+            event.target.nextElementSibling.classList.remove("filled");
+        }
+    });
+});
